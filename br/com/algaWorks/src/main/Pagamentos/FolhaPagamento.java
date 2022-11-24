bd196dcdb5b0d480cfcbf74cdaac490928051b8b
diazpedrodev <081diazpedro@gmail.com>
2022-11-24 14:13:44 -0300

    Pagamentos

---
+++ br/com/algaWorks/src/main/Pagamentos/FolhaPagamento.java
@@ -0,0 +1,12 @@
+package br.com.algaWorks.src.main.Pagamentos;
+
+public class FolhaPagamento {
+
+    double calcularSalario(int horasNormais, int horasExtras, double valorHoraNormal, double valorHoraExtra) {
+
+        double valorHorasNormais = horasNormais * valorHoraNormal;
+        double valorHorasExtras = horasExtras * valorHoraExtra;
+
+        return valorHorasNormais + valorHorasExtras;
+    }
+}
