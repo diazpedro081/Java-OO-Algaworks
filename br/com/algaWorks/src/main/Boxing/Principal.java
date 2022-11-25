fc155787aecb0825e734d313c14a52387f3dee6f
diazpedrodev <081diazpedro@gmail.com>
2022-11-25 14:39:25 -0300

    Boxing

---
+++ br/com/algaWorks/src/main/Boxing/Principal.java
@@ -0,0 +1,21 @@
+package br.com.algaWorks.src.main.Boxing;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        Integer i1 = 127;
+        Integer i2 = 127;
+
+        Integer i3 = 128;
+        Integer i4 = 128;
+
+        // ERRADO
+        System.out.println(i1 == i2);
+        System.out.println(i3 == i4);
+
+        // CORRETO
+        System.out.println(i1.equals(i2));
+        System.out.println(i3.equals(i4));
+
+    }
+}
