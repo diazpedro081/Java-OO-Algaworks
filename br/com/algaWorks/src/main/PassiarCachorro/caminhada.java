689872d21e405a4f3f6e453164a28892ddceb036
diazpedrodev <081diazpedro@gmail.com>
2022-11-28 12:19:15 -0300

    Delet

--- br/com/algaWorks/src/main/PassiarCachorro/caminhada.java
+++
@@ -1,8 +0,0 @@
-package br.com.algaWorks.src.main.PassiarCachorro;
-
-public class caminhada {
-    public void andar(Pessoa pessoa) {
-        System.out.println(pessoa.tutor.getNome() +
-                " esta andando com o " + pessoa.cachorro.getNome());
-    }
-}
