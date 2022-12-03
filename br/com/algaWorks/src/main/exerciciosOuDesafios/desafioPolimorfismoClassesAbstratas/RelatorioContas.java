685cc4f0c4983c13bc65a5aef82c042067740785
diazpedrodev <081diazpedro@gmail.com>
2022-12-03 18:35:30 -0300

    desafio polimorfismo estrutura relatorio contas

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/RelatorioContas.java
@@ -0,0 +1,7 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPolimorfismoClassesAbstratas;
+
+public class RelatorioContas {
+    public void exibirListagem(Conta[] contas) {
+        // codifique a listagem e detalhamento aqui
+    }
+}
