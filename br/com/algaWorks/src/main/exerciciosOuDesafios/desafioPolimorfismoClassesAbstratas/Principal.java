b255f06875b52453452d293c7fd42e7f6af6db7d
diazpedrodev <081diazpedro@gmail.com>
2022-12-03 18:35:12 -0300

    desafio polimorfismo estrutura main

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/Principal.java
@@ -0,0 +1,7 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPolimorfismoClassesAbstratas;
+
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
