944f569d90f913da709b1b858c1e0e8c0256561e
diazpedrodev <081diazpedro@gmail.com>
2022-12-04 15:48:03 -0300

    Exercicio Interfaces e Polimorfismo: Seguravel

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/exercicioInterfacesPolimorfismo/Seguravel.java
@@ -0,0 +1,8 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.exercicioInterfacesPolimorfismo;
+
+public interface Seguravel {
+    
+    public double calcularValorApolice();
+	public String obterDescricao();
+    
+}
