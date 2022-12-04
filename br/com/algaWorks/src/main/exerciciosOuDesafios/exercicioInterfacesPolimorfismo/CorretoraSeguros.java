c4f0a1a7df1503082180b90bbc3f492fa8abea3f
diazpedrodev <081diazpedro@gmail.com>
2022-12-04 15:47:28 -0300

    Exercicio Interfaces e Polimorfismo: Corretora Seguros

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/exercicioInterfacesPolimorfismo/CorretoraSeguros.java
@@ -0,0 +1,12 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.exercicioInterfacesPolimorfismo;
+
+public class CorretoraSeguros {
+    public void fazerPropostaSeguro(Seguravel objetoSeguravel) {
+        System.out.println("-------------------------------");
+        System.out.println("Corretora de Seguros - Proposta");
+        System.out.println("-------------------------------");
+        System.out.println(objetoSeguravel.obterDescricao());
+        System.out.println("Valor da apólice: " + objetoSeguravel.calcularValorApolice());
+        System.out.println("-------------------------------\n");
+    }
+}
