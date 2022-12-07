e404a605c139f35055070c0b83958f6112c2ef9c
diazpedrodev <081diazpedro@gmail.com>
2022-12-07 10:01:03 -0300

    Aulas.polimorfismo conta poupanca

---
+++ br/com/algaWorks/src/main/Aulas/polimorfismo/ContaPoupanca.java
@@ -0,0 +1,11 @@
+package br.com.algaWorks.src.main.Aulas.polimorfismo;
+
+public class ContaPoupanca extends Conta {
+    
+    private double rendimentos = 90;
+
+    public double getRendimentos() {
+        return rendimentos;
+    }
+
+}
