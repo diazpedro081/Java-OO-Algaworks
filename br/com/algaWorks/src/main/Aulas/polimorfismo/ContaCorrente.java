5e050454dd259021e5c56befba7942e203a58264
diazpedrodev <081diazpedro@gmail.com>
2022-12-07 10:00:52 -0300

    Aulas.polimorfismo conta corrente

---
+++ br/com/algaWorks/src/main/Aulas/polimorfismo/ContaCorrente.java
@@ -0,0 +1,11 @@
+package br.com.algaWorks.src.main.Aulas.polimorfismo;
+
+public class ContaCorrente extends Conta {
+    
+    private double limite = 1000;
+
+    public double getLimite() {
+        return limite;
+    }
+
+}
