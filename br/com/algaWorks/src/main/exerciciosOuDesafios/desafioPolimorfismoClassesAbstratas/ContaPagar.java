065442c5b8db959c6bdd03e4e5f71e4f0f539ffd
diazpedrodev <081diazpedro@gmail.com>
2022-12-09 09:35:54 -0300

    desafio Polimorfismo e Classe sAbstratas Conta a pagar

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/ContaPagar.java
@@ -0,0 +1,15 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPolimorfismoClassesAbstratas;
+
+public class ContaPagar extends Conta {
+
+    public ContaPagar(){}
+
+    public ContaPagar(Fornecedor fornecedor, String descricao, double valor, String data) {
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        
+    }
+    
+}
