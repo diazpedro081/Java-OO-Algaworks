ea8c43c22de84e28086db1766150f95289ff1071
diazpedrodev <081diazpedro@gmail.com>
2022-12-08 22:31:07 -0300

    Estudos Math teste

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioClasseJavaLangMath/teste/TesteAposta.java
@@ -0,0 +1,11 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioClasseJavaLangMath.teste;
+
+import br.com.algaWorks.src.main.exerciciosOuDesafios.desafioClasseJavaLangMath.model.Aposta;
+
+public class TesteAposta {
+    public static void main(String[] args) {
+        Aposta aposta = new Aposta();
+        System.out.println(aposta);
+        aposta.geradorDeNumerosMega();
+    }
+}
