f9a1709243b7ab6e0e6baaa1feb41157a7a4a1bf
diazpedrodev <081diazpedro@gmail.com>
2022-12-08 22:30:10 -0300

    Estudos Math interface pessoa

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioClasseJavaLangMath/interfaces/Pessoa.java
@@ -0,0 +1,8 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioClasseJavaLangMath.interfaces;
+
+public interface Pessoa {
+    
+    abstract String getNome();
+    abstract int getId();
+    
+}
