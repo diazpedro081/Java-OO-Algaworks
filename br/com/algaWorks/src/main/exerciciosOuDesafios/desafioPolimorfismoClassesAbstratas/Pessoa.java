1212616b07d169e14edeffb3213a9f03a06dd8a2
diazpedrodev <081diazpedro@gmail.com>
2022-12-09 09:36:22 -0300

    desafio Polimorfismo e Classe sAbstratas Pessoa

--- br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/Pessoa.java
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/Pessoa.java
@@ -2,4 +2,30 @@ package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPolimorfismoClasse
 
 public abstract class Pessoa {
     
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
 }
