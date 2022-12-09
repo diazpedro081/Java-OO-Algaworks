e9e31fa03265356531eb47e2580cc4d7039b9e7b
diazpedrodev <081diazpedro@gmail.com>
2022-12-09 09:36:02 -0300

    desafio Polimorfismo e Classe sAbstratas Conta a receber

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/ContaReceber.java
@@ -0,0 +1,16 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPolimorfismoClassesAbstratas;
+
+public class ContaReceber extends Conta {
+
+    @Override
+    public void exibirDetalhes() {
+        
+    }
+
+    public ContaReceber() {
+    }
+    
+    public ContaReceber(Cliente cliente, String descricao, double valor, String data) {
+    }
+    
+}
