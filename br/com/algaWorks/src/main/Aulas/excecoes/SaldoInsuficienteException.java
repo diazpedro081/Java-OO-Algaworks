4372e2271c08f3801727c8e0e1a5df3b161df7a0
diazpedrodev <081diazpedro@gmail.com>
2022-12-09 12:33:51 -0300

    SaldoInsuficienteException

---
+++ br/com/algaWorks/src/main/Aulas/excecoes/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package br.com.algaWorks.src.main.Aulas.excecoes;
+
+public class SaldoInsuficienteException extends Exception {
+    
+    public SaldoInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
