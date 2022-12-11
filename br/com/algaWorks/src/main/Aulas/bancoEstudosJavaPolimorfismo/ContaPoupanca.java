63d7950393c2c2a41b1d06f1bf14be3892e7a241
diazpedrodev <081diazpedro@gmail.com>
2022-12-11 07:08:03 -0300

    Banco Estudos Java Polimorfismo: Conta Poupanca

---
+++ br/com/algaWorks/src/main/Aulas/bancoEstudosJavaPolimorfismo/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package br.com.algaWorks.src.main.Aulas.bancoEstudosJavaPolimorfismo;
+
+public class ContaPoupanca extends Conta {
+
+    public double getRendimentos() {
+        return rendimentos;
+    }
+
+}
