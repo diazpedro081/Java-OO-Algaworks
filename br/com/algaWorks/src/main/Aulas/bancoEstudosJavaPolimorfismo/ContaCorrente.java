0686e7bc96d838e7de3f6c1d257322aea3ada23b
diazpedrodev <081diazpedro@gmail.com>
2022-12-11 07:05:30 -0300

    Banco Estudos Java Polimorfismo: Conta corrente

---
+++ br/com/algaWorks/src/main/Aulas/bancoEstudosJavaPolimorfismo/ContaCorrente.java
@@ -0,0 +1,21 @@
+package br.com.algaWorks.src.main.Aulas.bancoEstudosJavaPolimorfismo;
+
+public class ContaCorrente extends Conta {
+
+    public double getLimite() {
+        return limite;
+    }
+    
+    public ContaCorrente() {
+    }
+
+    public ContaCorrente(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Saldo da Conta Corrente: R$" + saldo;
+    }
+
+}
