5e52e1ffe550896c70fc25d24ad5030b5e562c98
diazpedrodev <081diazpedro@gmail.com>
2022-12-12 10:28:20 -0300

    Desafio exceções: Pessoa

--- br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/Pessoa.java
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/Pessoa.java
@@ -1,31 +1,14 @@
 package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPolimorfismoClassesAbstratas;
 
-public abstract class Pessoa {
-    
-    private String nome;
-    private String cpf;
-    private int idade;
+public class Pessoa {
+	
+	public String nome;
 
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    
-    public int getIdade() {
-        return idade;
-    }
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
