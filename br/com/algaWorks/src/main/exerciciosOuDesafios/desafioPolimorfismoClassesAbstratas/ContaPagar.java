ab5cf83e2fd9d8dc36f4a0ae61876fc7136c9ebb
diazpedrodev <081diazpedro@gmail.com>
2022-12-12 10:27:53 -0300

    Desafio exceções: Conta pagar

--- br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/ContaPagar.java
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioPolimorfismoClassesAbstratas/ContaPagar.java
@@ -1,15 +1,43 @@
 package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPolimorfismoClassesAbstratas;
 
+import br.com.algaWorks.src.main.exerciciosOuDesafios.desafioJavaExcecoes.OperacaoContaException;
+import br.com.algaWorks.src.main.exerciciosOuDesafios.desafioPacotesEnumeracoes.financeiro.modelo.SituacaoConta;
+
 public class ContaPagar extends Conta {
 
-    public ContaPagar(){}
+	private Fornecedor fornecedor;
 
-    public ContaPagar(Fornecedor fornecedor, String descricao, double valor, String data) {
-    }
+	public ContaPagar() {
+	}
 
-    @Override
-    public void exibirDetalhes() {
-        
-    }
-    
+	public ContaPagar(Fornecedor fornecedor, String descricao, Double valor, String dataVencimento) {
+		this.fornecedor = fornecedor;
+		this.setDescricao(descricao);
+		this.setValor(valor);
+		this.setDataVencimento(dataVencimento);
+	}
+
+	public void pagar() throws OperacaoContaException {
+		if (SituacaoConta.PAGA.equals(this.getSituacaoConta())) {
+			throw new OperacaoContaException("Não pode pagar uma conta que já está paga: " 
+				+ this.getDescricao() + ".");
+		} else if (SituacaoConta.CANCELADA.equals(this.getSituacaoConta())) {
+			throw new OperacaoContaException("Não pode pagar uma conta que está cancelada: " 
+				+ this.getDescricao() + ".");
+		} else {
+			System.out.println("Pagando conta " + this.getDescricao() + " no valor de " 
+				+ this.getValor() + " e vencimento em " + this.getDataVencimento() 
+				+ " do fornecedor " + this.getFornecedor().getNome() + ".");
+				
+			this.situacaoConta = SituacaoConta.PAGA;
+		}
+	}
+
+	public Fornecedor getFornecedor() {
+		return this.fornecedor;
+	}
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
 }
