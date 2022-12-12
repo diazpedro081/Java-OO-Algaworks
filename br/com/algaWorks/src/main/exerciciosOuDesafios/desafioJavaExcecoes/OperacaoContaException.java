d460aaa533e951defa4f4f16fbde2041e875ad12
diazpedrodev <081diazpedro@gmail.com>
2022-12-12 10:27:45 -0300

    Desafio exceções:  Operacao Conta exception

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioJavaExcecoes/OperacaoContaException.java
@@ -0,0 +1,8 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioJavaExcecoes;
+
+public class OperacaoContaException extends Exception{
+    
+    public OperacaoContaException(String msg) {
+        super(msg);
+    }
+}
