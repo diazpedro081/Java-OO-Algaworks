8340027fa4505763b458f95ab5d13b644d1ad01e
diazpedrodev <081diazpedro@gmail.com>
2022-12-15 11:49:45 -0300

    Estaudos Collections: MapHashMap

---
+++ br/com/algaWorks/src/main/Aulas/collections/TesteMapHashMap.java
@@ -0,0 +1,32 @@
+package br.com.algaWorks.src.main.Aulas.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TesteMapHashMap {
+    
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno("João");
+        Aluno a2 = new Aluno("Maria");
+        Aluno a3 = new Aluno("Jose");
+        Aluno a4 = new Aluno("Pedro");
+
+        Map<String, Aluno> alunos = new HashMap<String, Aluno>();
+
+        alunos.put("1", a1);
+        alunos.put("2", a2);
+        alunos.put("3", a3);
+        alunos.put("4", a4);
+
+        imprimirAlunos(alunos);
+
+        Aluno alunoRecuperado = alunos.get("3");
+        System.out.println("aAluno de chave 3: " + alunoRecuperado.getNome());
+    }
+
+    private static void imprimirAlunos(Map<String, Aluno> alunos) {
+        for(Aluno a : alunos.values()) {
+            System.out.println("Nome: " + a.getNome());
+        }
+    }
+}
