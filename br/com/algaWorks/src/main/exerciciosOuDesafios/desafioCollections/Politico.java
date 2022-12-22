14cce96ab91a73faa8ce9e3d9d73428094211b7f
diazpedrodev <081diazpedro@gmail.com>
2022-12-22 09:22:03 -0300

    Desafio: collections Class Politico

---
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioCollections/Politico.java
@@ -0,0 +1,22 @@
+package br.com.algaWorks.src.main.exerciciosOuDesafios.desafioCollections;
+
+public class Politico {
+    
+    private String nome;
+    private Cargo cargo;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+}
