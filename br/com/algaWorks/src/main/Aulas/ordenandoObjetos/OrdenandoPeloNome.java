121c2f0579adc21a3ab77fae1e333a08d6759029
diazpedrodev <081diazpedro@gmail.com>
2022-12-23 08:56:43 -0300

    comparator and comparable java

---
+++ br/com/algaWorks/src/main/Aulas/ordenandoObjetos/OrdenandoPeloNome.java
@@ -0,0 +1,23 @@
+package br.com.algaWorks.src.main.Aulas.ordenandoObjetos;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.algaWorks.src.main.Aulas.construtores.Pessoa;
+
+public class OrdenandoPeloNome {
+    
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("João", 22);
+        Pessoa p2 = new Pessoa("Adriano", 23);
+        Pessoa p3 = new Pessoa("Bruno", 21);
+
+        List<Pessoa> pessoas = Arrays.asList(p1, p2, p3);
+        Collections.sort(pessoas);
+
+        for(Pessoa pessoa : pessoas) {
+            System.out.println(pessoa.getNome());
+        }
+    }
+}
