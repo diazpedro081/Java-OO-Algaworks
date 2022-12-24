8b5883e4fcd5a0759f41cca823c0574e54a97266
diazpedrodev <081diazpedro@gmail.com>
2022-12-24 14:13:41 -0300

    exercicio arrays

--- br/com/algaWorks/src/main/ExerciciosOuDesafios/ExercicioArrays/Verifica.java
+++ br/com/algaWorks/src/main/ExerciciosOuDesafios/ExercicioArrays/Verifica.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class Verifica {
 
     void ListarProdutos(int listaProdutos) {
-        Scanner entrada = new Scanner(System.in);
+        try (Scanner entrada = new Scanner(System.in)) {
+            Produtos[] produtos = new Produtos[listaProdutos];
 
-        Produtos[] produtos = new Produtos[listaProdutos];
+            for (int n = 0; n < produtos.length; n++) {
+                if (n != produtos.length) {
+                    produtos[n] = new Produtos();
 
-        for (int n = 0; n < produtos.length; n++) {
-            if (n != produtos.length) {
-                produtos[n] = new Produtos();
+                    System.out.print("Qual o nome do produto: ");
+                    String nome = entrada.next();
+                    produtos[n].setNomeProduto(nome);
 
-                System.out.print("Qual o nome do produto: ");
-                String nome = entrada.next();
-                produtos[n].setNomeProduto(nome);
+                    System.out.print("Digite a quantidade do produto " + nome + ": ");
+                    int quantidade = entrada.nextInt();
+                    produtos[n].setQuantidadeProduto(quantidade);
 
-                System.out.print("Digite a quantidade do produto " + nome + ": ");
-                int quantidade = entrada.nextInt();
-                produtos[n].setQuantidadeProduto(quantidade);
+                    System.out.println(produtos[n]);
+                    
+                } 
 
-                System.out.println(produtos[n]);
-                
-            } 
-
+            }
         }
 
         System.out.println("A lista foi Encerrada.");
