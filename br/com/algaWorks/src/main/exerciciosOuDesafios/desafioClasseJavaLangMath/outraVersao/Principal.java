412d88af8e884e11f7f05e2c8b19072634045bf3
diazpedrodev <081diazpedro@gmail.com>
2022-12-26 09:04:56 -0300

    desafio Math

--- br/com/algaWorks/src/main/exerciciosOuDesafios/desafioClasseJavaLangMath/outraVersao/Principal.java
+++ br/com/algaWorks/src/main/exerciciosOuDesafios/desafioClasseJavaLangMath/outraVersao/Principal.java
@@ -6,18 +6,18 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.print("Quantos jogos? ");
+            int quantidadeJogos = entrada.nextInt();
 
-        System.out.print("Quantos jogos? ");
-        int quantidadeJogos = entrada.nextInt();
-
-        System.out.println("----------------------------");
-
-        for (int i = 1; i <= quantidadeJogos; i++) {
-            MegaSena mega = new MegaSena();
-            mega.sortear();
-            mega.exibirNumeros();
             System.out.println("----------------------------");
+
+            for (int i = 1; i <= quantidadeJogos; i++) {
+                MegaSena mega = new MegaSena();
+                mega.sortear();
+                mega.exibirNumeros();
+                System.out.println("----------------------------");
+            }
         }
 
         System.out.println("*** Boa sorte! ***");
