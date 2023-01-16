cbd0a066df01de9bb55a243e46f083576b702014
diazpedrodev <081diazpedro@gmail.com>
2023-01-16 09:02:56 -0300

    wstudos xml

---
+++ br/com/algaWorks/src/main/Aulas/estudosXML/src/SalvandoProduto.java
@@ -0,0 +1,26 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import modelo.Produto;
+
+public class SalvandoProduto {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        XStream xStream = new XStream(new DomDriver());
+        xStream.alias("produto", Produto.class);
+        xStream.aliasAttribute(Produto.class, "codigo", "codigo");
+
+        Produto sabonete = new Produto(1L, "sabonete", new BigDecimal("2.30"));
+
+        String xml = xStream.toXML(sabonete);
+        System.out.println(xml);
+
+        OutputStream outputStream = new FileOutputStream("./produto2.xml");
+        xStream.toXML(sabonete, outputStream);
+    }
+}
