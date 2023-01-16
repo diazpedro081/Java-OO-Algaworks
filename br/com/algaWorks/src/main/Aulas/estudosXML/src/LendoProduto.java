14002e86003d0a1f2d74cdb5d1d48cd1b9633b2a
diazpedrodev <081diazpedro@gmail.com>
2023-01-16 09:03:16 -0300

    Estudos xml

---
+++ br/com/algaWorks/src/main/Aulas/estudosXML/src/LendoProduto.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import modelo.Produto;
+
+public class LendoProduto {
+    public static void main(String[] args) throws FileNotFoundException {
+		XStream xstream = new XStream(new DomDriver());
+		xstream.alias("produto", Produto.class);
+		xstream.aliasAttribute(Produto.class, "codigo", "codigo");
+		
+		Produto produto = (Produto) xstream.fromXML(new FileInputStream("./produto2.xml"));
+		System.out.println(produto.getCodigo());
+		System.out.println(produto.getDescricao());
+		System.out.println(produto.getValor());
+	}
+}
