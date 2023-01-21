f19e57a7f6ae3bc7164aa5fa368170a95166b83d
diazpedrodev <081diazpedro@gmail.com>
2023-01-21 09:07:02 -0300

    Create Client.java

---
+++ br/com/algaWorks/src/main/Aulas/estudosJDBC/testJDBC/src/domain/model/Client.java
@@ -0,0 +1,22 @@
+package domain.model;
+
+public class Client {
+
+	private Long code;
+	private String name;
+	
+	public Long getCode() {
+		return code;
+	}
+	public void setCode(Long code) {
+		this.code = code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
