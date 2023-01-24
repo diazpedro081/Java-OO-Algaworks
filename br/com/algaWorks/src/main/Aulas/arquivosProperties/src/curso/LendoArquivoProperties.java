37b40979b47a5fbe80722edbf0a20f5f6659b779
diazpedrodev <081diazpedro@gmail.com>
2023-01-23 21:55:54 -0300

    Create LendoArquivoProperties.java

---
+++ br/com/algaWorks/src/main/Aulas/arquivosProperties/src/curso/LendoArquivoProperties.java
@@ -0,0 +1,27 @@
+package curso;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class LendoArquivoProperties {
+    
+    public static void main(String[] args) throws Exception {
+        Properties prop = new Properties();
+        prop.load(new FileInputStream("./config/config.properties"));
+
+        // Conectar no banco de dados...
+        String url = prop.getProperty("banco.dados.url");
+        String usuario = prop.getProperty("banco.dados.usuario");
+        String senha = prop.getProperty("banco.dados.senha");
+
+        System.out.printf("Conectando no banco de dados de url: %s e usuario: %$\n", url, usuario);
+
+        try {
+            int x = 5 / 0;
+        } catch (Exception e) {
+            // Se acontecer algum erro, voce precisa informar o adiministrador
+            String email = prop.getProperty("email.admin");
+            System.err.printf("Enviado email para: %s informando o erro: %s", email, e.getMessage());
+        }
+    }
+}
