74ad87a298781a293a74246c62118f4954f86e48
diazpedrodev <081diazpedro@gmail.com>
2023-01-24 21:20:40 -0300

    Update LendoArquivoProperties.java

--- br/com/algaWorks/src/main/Aulas/arquivosProperties/src/curso/LendoArquivoProperties.java
+++ br/com/algaWorks/src/main/Aulas/arquivosProperties/src/curso/LendoArquivoProperties.java
@@ -12,12 +12,13 @@ public class LendoArquivoProperties {
         // Conectar no banco de dados...
         String url = prop.getProperty("banco.dados.url");
         String usuario = prop.getProperty("banco.dados.usuario");
-        String senha = prop.getProperty("banco.dados.senha");
+        //String senha = prop.getProperty("banco.dados.senha");
 
         System.out.printf("Conectando no banco de dados de url: %s e usuario: %$\n", url, usuario);
 
         try {
             int x = 5 / 0;
+            System.out.println(x);
         } catch (Exception e) {
             // Se acontecer algum erro, voce precisa informar o adiministrador
             String email = prop.getProperty("email.admin");
