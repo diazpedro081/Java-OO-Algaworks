d7a350afe4159490269518ce811009ef2f218a1a
diazpedrodev <081diazpedro@gmail.com>
2023-03-07 20:15:57 -0300

    Create AtivacaoClienteService.java

---
+++ br/com/algaWorks/src/main/Aulas/injecaoDependencia/src/main/java/com/di/service/AtivacaoClienteService.java
@@ -0,0 +1,19 @@
+package com.di.service;
+
+import com.di.modelo.Cliente;
+import com.di.notificacao.Notificador;
+
+public class AtivacaoClienteService {
+
+    private Notificador notificador;
+
+    public AtivacaoClienteService(Notificador notificador) {
+        this.notificador = notificador;
+    }
+
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+
+        notificador.notificar(cliente, "Seu cadastro esta ativo!");
+    }
+}
